a73a9d4f3db19e65d6e3b21876cb72786446e0f3
Mr-Bie <mrb800424@gmail.com>
2023-08-21 20:53:27 +0330

    FIX: error handler not showing internal server error

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -2,8 +2,10 @@ import logger from "../services/logger.service";
 
 export const errorHandlerMiddleware = (err, req, res, next) => {
   const statusCode = err?.status || 500;
-  logger.error(JSON.stringify(err));
-  return res.status(statusCode).json({error: err.message, timestamps: new Date()});
+  logger.error(err.message);
+  let message = "Internal Server Error!";
+  if (statusCode !== 500) message = err.message;
+  return res.status(statusCode).json({error: message, timestamps: new Date()});
 };
 
 export class HttpException extends Error {
