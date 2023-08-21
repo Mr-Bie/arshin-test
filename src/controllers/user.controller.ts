abcac9e8be3d942ef801eb2e52423ee1d62d40c9
Mr-Bie <mrb800424@gmail.com>
2023-08-21 14:53:31 +0330

    FEAT: get all users endpoint added

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,3 +4,7 @@ import {CreateUserDto} from "../dto/User.dto";
 export const createUser = async (data: CreateUserDto) => {
     return prisma.user.create({data: {email: data.email, name: data.name}});
 }
+
+export const getAllUsers = () => {
+    return prisma.user.findMany({include: {expenses: true}});
+}
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -5,9 +5,25 @@ import {CreateUserDto} from "../dto/User.dto";
 
 const userRouter: Router = Router();
 
-userRouter.post("/create", validatorMiddleware(CreateUserDto) ,async (req,res) => {
-    const user = await userController.createUser(req.body as CreateUserDto);
+userRouter.post("/create", validatorMiddleware(CreateUserDto) ,async (req,res, next) => {
+    let user;
+    try {
+        user = await userController.createUser(req.body as CreateUserDto);
+    } catch(err) {
+        return next(err);
+    }
     res.status(201).json({data: {user}, message: "User Created Successfully!",})
 });
+
+userRouter.get("/find", async (req,res, next) => {
+    let users;
+    try {
+        users = await userController.getAllUsers();
+    } catch(err) {
+        return next(err);
+    }
+
+    return res.status(200).json({message: "all users fetched!", data: {users}})
+})
 
 export default userRouter;
